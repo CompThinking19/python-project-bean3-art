945531bb09e0b5ebb00e5de8a426779032c3b845
Claudine Raben <clr196@scarletmail.rutgers.edu>
2019-11-25 21:34:36 -0500

    Final Python Project submisson

--- pythonproject.py
+++ pythonproject.py
@@ -1,45 +1,42 @@
 import random
 #This code will be used as a healthy meal generator for breakfast, lunch, dinner, and snacks.
 #Code will build a meal taking food choices from variables.
+#Food choices come from the Harvard Healthy Eating Food Pyramid.
 breakfast = {
-'whole_grains':['wheat toast', 'quinoa', 'oatmeal', 'whole grain toast', 'whole grain cereal', 'whole grain bagel', 'whole grain english muffin'],
-'fruits': ['apples', 'oranges', 'grapefruit', 'berries', 'grapes', 'melon', 'banana', 'peaches'],
-'dairy': ['skim milk', 'plain yogurt', 'cheese'],
-'lean_protein': ['eggs', 'turkey bacon', 'turkey sausage', 'nuts'],
-'vegetables': ['broccoli', 'cauliflower', 'tomato', 'carrots', 'spinach', 'zucchini', 'mushrooms', 'lettuce', 'asparagus', 'celery', 'peppers']
+'whole_grains':['Wheat Toast', 'Quinoa', 'Oatmeal', 'Whole Grain Toast', 'Whole Grain cereal', 'Whole Grain Bagel', 'Whole Grain English Muffin'],
+'fruits': ['Apples', 'Oranges', 'Grapefruit', 'Berries', 'Grapes', 'Melon', 'Banana', 'Peaches'],
+'dairy': ['Skim Milk', 'Plain Greek Yogurt', 'Low-fat Cream Cheese'],
+'lean_protein': ['Eggs', 'Turkey Bacon', 'Turkey Sausage', 'Nuts'],
+'vegetables': ['Broccoli', 'Tomato', 'Spinach', 'Zucchini', 'Mushrooms', 'Asparagus', 'Peppers']
 }
 lunch = {
-'whole_grains': ['whole grain bread', 'brown rice', 'whole wheat pasta', 'quinoa'],
-'fruits': ['oranges', 'apples', 'berries', 'grapes', 'melon', 'mango', 'peaches', 'banana', 'kiwi'],
-'vegetables': ['broccoli', 'cauliflower', 'tomato', 'carrots', 'spinach', 'zucchini', 'mushrooms', 'lettuce', 'asparagus', 'celery', 'peppers'],
-'lean_protein': ['fish', 'chicken', 'shellfish', 'nuts', 'turkey', 'tofu', 'beans', 'eggs', 'turkey bacon', 'turkey sausage'],
-'dairy': ['plain yogurt', 'cheese', 'skim milk']
+'whole_grains': ['Whole Grain Bread', 'Brown Rice', 'Whole Wheat Pasta', 'Quinoa'],
+'fruits': ['Oranges', 'Apples', 'Berries', 'Grapes', 'Melon', 'Mango', 'Peaches', 'Banana', 'Kiwi'],
+'vegetables': ['Broccoli', 'Cauliflower', 'Tomato', 'Carrots', 'Spinach', 'Zucchini', 'Mushrooms', 'Lettuce', 'Asparagus', 'Celery', 'Peppers'],
+'lean_protein': ['Fish', 'Chicken', 'Shellfish', 'Nuts', 'Turkey', 'Tofu', 'Beans', 'Hard Boiled Egg', 'Turkey Bacon'],
+'dairy': ['Plain Greek Yogurt', 'Low-fat Cheese', 'Skim Milk']
 }
 dinner = {
-'lean_protein':['fish', 'chicken', 'shellfish', 'nuts', 'turkey', 'tofu', 'beans'],
-'vegetables': ['broccoli', 'cauliflower', 'tomato', 'carrots', 'spinach', 'zucchini', 'mushrooms', 'lettuce', 'asparagus', 'celery', 'peppers'],
-'dairy': ['cheese', 'plain yogurt', 'skim milk'],
-'fruits': ['oranges', 'apples', 'berries', 'grapes', 'melon', 'mango', 'peaches', 'banana', 'kiwi'],
-'whole_grains': ['whole grain bread', 'brown rice', 'whole wheat pasta', 'quinoa']
+'lean_protein':['Fish', 'Chicken', 'Shellfish', 'Nuts', 'Turkey', 'Tofu', 'Beans'],
+'vegetables': ['Broccoli', 'Cauliflower', 'Tomato', 'Carrots', 'Spinach', 'Zucchini', 'Mushrooms', 'Lettuce', 'Asparagus', 'Celery', 'Peppers'],
+'dairy': ['Low-fat Cheese', 'Plain Greek Yogurt', 'Skim Milk'],
+'fruits': ['Oranges', 'Apples', 'Berries', 'Grapes', 'Melon', 'Mango', 'Peaches', 'Banana', 'Kiwi'],
+'whole_grains': ['Whole Grain Bread', 'Brown Rice', 'Whole Wheat Pasta', 'Quinoa']
 }
 snack = {
-'fruits':['oranges', 'apples', 'berries', 'grapes', 'melon', 'mango', 'peaches', 'banana', 'kiwi'],
-'whole_grains':['bran muffin', 'popcorn', 'whole wheat crackers', 'whole wheat toast'],
-'vegetables':['carrots', 'celery', 'peppers', 'tomatoes', 'broccoli', 'cauliflower'],
-'dairy': ['cheese', 'plain yogurt', 'skim milk'],
-'lean_protein':['fish', 'chicken', 'shellfish', 'nuts', 'turkey', 'tofu', 'beans']
+'fruits':['Oranges', 'Apples', 'Berries', 'Grapes', 'Melon', 'Mango', 'Peaches', 'Banana', 'Kiwi'],
+'whole_grains':['Bran Muffin', 'Popcorn', 'Whole Wheat Crackers', 'Whole Wheat Toast'],
+'vegetables':['Carrots', 'Celery', 'Peppers', 'Tomatoes', 'Broccoli'],
+'dairy': ['Low-fat Cheese Cubes', 'Plain Greek Yogurt', 'Skim Milk'],
+'lean_protein':['Hummus', 'Chicken', 'Nuts', 'Turkey', 'Tofu']
 }
-#This function takes the string 'meal' as a variable and will return a single meal for either breakfast, lunch, dinner, and snacks.
+#This function takes ingredients as a variable and will return a single meal for either breakfast, lunch, dinner, or snack.
 def meal_plan(ingredients):
-    breakfast_dict = {'[]'}
-    lunch_dict = {'[]'}
-    dinner_dict = {'[]'}
-    snack_dict = {'[]'}
-    for breakfast_dict in random.sample(breakfast_dict.items(),k=5):
-        print breakfast_dict
-    for lunch_dict in random.sample(lunch_dict.items(),k=5):
-        print lunch_dict
-    for dinner_dict in random.sample(dinner_dict.items(),k=5):
-        print dinner_dict
-    for snack_dict in random.sample(snack_dict.items(),k=5):
-        print snack_dict
+    food = ' '
+    meal = ' & '.join(food)
+    for meal in ingredients:
+        food += random.choice(ingredients[meal])+' & '
+    return food
+print ''.join(meal_plan(dinner))
+#When user inputs meal choice whether breakfast, lunch, dinner, or snack, code will return a random meal for them.
+#Printed meal will consist of a fruit, a vegetable, a whole_grain, a dairy product, and a lean_protein.
